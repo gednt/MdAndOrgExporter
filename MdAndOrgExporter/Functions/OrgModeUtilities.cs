9231a09535382c5b8b241eb2e56e675f9fd382a7
gednt <big.gamesedicas@gmail.com>
2024-09-14 15:14:39 -0300

    V0.2.1 - Better paragraph management

--- MdAndOrgExporter/Functions/OrgModeUtilities.cs
+++ MdAndOrgExporter/Functions/OrgModeUtilities.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using ExtensionMethods;
+using Microsoft.Office.Interop.Word;
 namespace MdAndOrgExporter.Functions
 {
     public class OrgModeUtilities
@@ -25,56 +26,56 @@ namespace MdAndOrgExporter.Functions
         public String CreateHeading(List<Paragraph>Paragraphs)
         {
             var textToReturn = new StringBuilder();
-            foreach (var paragraph in Paragraphs)
+            for (int cont=0;cont<Paragraphs.Count;cont++)
             {
-                switch (paragraph.ParagraphFormat.OutlineLevel)
+                switch (Paragraphs[cont].ParagraphFormat.OutlineLevel)
                 {
                     case Microsoft.Office.Interop.Word.WdOutlineLevel.wdOutlineLevel1:
                         textToReturn.Append(textToReturn.ToString().ReturnIteratedChars(1, '*'));
                         break;
                     case Microsoft.Office.Interop.Word.WdOutlineLevel.wdOutlineLevel2:
-                        textToReturn.Append(textToReturn.ToString().ReturnIteratedChars(2 + (int)Math.Round(paragraph.Identation), '*'));
+                        textToReturn.Append(textToReturn.ToString().ReturnIteratedChars(cont>0 &&!Paragraphs[cont - 1].ParagraphFormat.Equals(Microsoft.Office.Interop.Word.WdOutlineLevel.wdOutlineLevel2) ? (int)Math.Round(Paragraphs[cont-1].Identation)+1: 1, Paragraphs[cont].IdentationCharacter));
                         break;
                     case Microsoft.Office.Interop.Word.WdOutlineLevel.wdOutlineLevel3:
-                        textToReturn.Append(textToReturn.ToString().ReturnIteratedChars(3 + (int)Math.Round(paragraph.Identation), '*'));
+                        textToReturn.Append(textToReturn.ToString().ReturnIteratedChars(cont > 0 && !Paragraphs[cont - 1].ParagraphFormat.Equals(Microsoft.Office.Interop.Word.WdOutlineLevel.wdOutlineLevel3) ? (int)Math.Round(Paragraphs[cont - 1].Identation) + 1 : 2, Paragraphs[cont].IdentationCharacter));
                         break;
                     case Microsoft.Office.Interop.Word.WdOutlineLevel.wdOutlineLevel4:
-                        textToReturn.Append(textToReturn.ToString().ReturnIteratedChars(4 + (int)Math.Round(paragraph.Identation), '*'));
+                        textToReturn.Append(textToReturn.ToString().ReturnIteratedChars(cont > 0 && !Paragraphs[cont - 1].ParagraphFormat.Equals(Microsoft.Office.Interop.Word.WdOutlineLevel.wdOutlineLevel4) ? (int)Math.Round(Paragraphs[cont - 1].Identation) + 2 : 3, Paragraphs[cont].IdentationCharacter));
                         break;
                     case Microsoft.Office.Interop.Word.WdOutlineLevel.wdOutlineLevel5:
-                        textToReturn.Append(textToReturn.ToString().ReturnIteratedChars(4 + (int)Math.Round(paragraph.Identation), '*'));
+                        textToReturn.Append(textToReturn.ToString().ReturnIteratedChars(cont > 0 && !Paragraphs[cont - 1].ParagraphFormat.Equals(Microsoft.Office.Interop.Word.WdOutlineLevel.wdOutlineLevel5) ? (int)Math.Round(Paragraphs[cont - 1].Identation) + 3 : 4, Paragraphs[cont].IdentationCharacter));
                         break;
                     case Microsoft.Office.Interop.Word.WdOutlineLevel.wdOutlineLevel6:
-                        textToReturn.Append(textToReturn.ToString().ReturnIteratedChars(4 + (int)Math.Round(paragraph.Identation), '*'));
+                        textToReturn.Append(textToReturn.ToString().ReturnIteratedChars(cont > 0 && !Paragraphs[cont - 1].ParagraphFormat.Equals(Microsoft.Office.Interop.Word.WdOutlineLevel.wdOutlineLevel6) ? (int)Math.Round(Paragraphs[cont - 1].Identation) + 4 : 4, Paragraphs[cont].IdentationCharacter));
                         break;
                     case Microsoft.Office.Interop.Word.WdOutlineLevel.wdOutlineLevel7:
-                        textToReturn.Append(textToReturn.ToString().ReturnIteratedChars(4 + (int)Math.Round(paragraph.Identation), '*'));
+                        textToReturn.Append(textToReturn.ToString().ReturnIteratedChars(cont > 0 && !Paragraphs[cont - 1].ParagraphFormat.Equals(Microsoft.Office.Interop.Word.WdOutlineLevel.wdOutlineLevel7) ? (int)Math.Round(Paragraphs[cont - 1].Identation) + 5 : 4, Paragraphs[cont].IdentationCharacter));
                         break;
                     case Microsoft.Office.Interop.Word.WdOutlineLevel.wdOutlineLevel8:
-                        textToReturn.Append(textToReturn.ToString().ReturnIteratedChars(4 + (int)Math.Round(paragraph.Identation), '*'));
+                        textToReturn.Append(textToReturn.ToString().ReturnIteratedChars(cont > 0 && !Paragraphs[cont - 1].ParagraphFormat.Equals(Microsoft.Office.Interop.Word.WdOutlineLevel.wdOutlineLevel8) ? (int)Math.Round(Paragraphs[cont - 1].Identation) + 6 : 4, Paragraphs[cont].IdentationCharacter));
                         break;
                     case Microsoft.Office.Interop.Word.WdOutlineLevel.wdOutlineLevel9:
-                        textToReturn.Append(textToReturn.ToString().ReturnIteratedChars(4 + (int)Math.Round(paragraph.Identation), '*'));
+                        textToReturn.Append(textToReturn.ToString().ReturnIteratedChars(cont > 0 && !Paragraphs[cont - 1].ParagraphFormat.Equals(Microsoft.Office.Interop.Word.WdOutlineLevel.wdOutlineLevel9) ? (int)Math.Round(Paragraphs[cont - 1].Identation) + 7 : 4, Paragraphs[cont].IdentationCharacter));
                         break;
                     case Microsoft.Office.Interop.Word.WdOutlineLevel.wdOutlineLevelBodyText:
-                        textToReturn.Append(textToReturn.ToString().ReturnIteratedChars(5 + (int)Math.Round(paragraph.Identation), '*'));
+                        textToReturn.Append(textToReturn.ToString().ReturnIteratedChars(cont > 0 && !Paragraphs[cont - 1].ParagraphFormat.Equals(Microsoft.Office.Interop.Word.WdOutlineLevel.wdOutlineLevelBodyText) ? (int)Math.Round(Paragraphs[cont-1].Identation)+1:5, Paragraphs[cont].IdentationCharacter));
                         break;
                     default:
                         break;
                 }
                 //for (int i = 0; i < levels; i++)
                 //{
-                //    textToReturn.Append(paragraph.IdentationCharacter);
+                //    textToReturn.Append(Paragraphs[cont].IdentationCharacter);
                 //}
-                if(paragraph.Text != "\r" && paragraph.Text != "/\r")
+                if(Paragraphs[cont].Text != "\r" && Paragraphs[cont].Text != "/\r")
                 {
-                    if (paragraph.ContainsImage == false)
+                    if (Paragraphs[cont].ContainsImage == false)
                     {
-                        textToReturn.Append(" " + (paragraph.ListFormat != null ? paragraph.ListFormat.ListString + " " : "") + paragraph.Text + " \n");
+                        textToReturn.Append(" " + (Paragraphs[cont].ListFormat != null ? Paragraphs[cont].ListFormat.ListString + " " : "") + Paragraphs[cont].Text + " \n");
                     }
                     else
                     {
-                        textToReturn.Append(" " + (paragraph.ListFormat != null ? paragraph.ListFormat.ListString + " " : "") + $"![{Path.GetFileName(paragraph.Text)}](../assets/{paragraph.Text})" + " \n");
+                        textToReturn.Append(" " + (Paragraphs[cont].ListFormat != null ? Paragraphs[cont].ListFormat.ListString + " " : "") + $"![{Path.GetFileName(Paragraphs[cont].Text)}](../assets/{Paragraphs[cont].Text})" + " \n");
                     }
                 }
 
